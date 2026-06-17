c1954f64cdeb7165db221068cffbe3da2f5b1d44
phantekzy <mainiloti@gmail.com>
2026-06-17 16:13:53 +0100

    fixed the type error

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+
 fn main() {
-    println!("Guess the number");
-
-    let secret_number = rand::thread_rng().gen_range(0, 101);
-    println!("the secret number is {}", secret_number);
-
+    println!("Password crack");
+    // Password generated
+    let password = rand::thread_rng().gen_range(0, 101);
+    // OPTIONAL : show the generated Password
+    println!("GENERATED PASSWORD : {}", password);
     loop {
-        println!("Enter the number");
+        println!("Enter the right password");
+        // Instance of the string
         let mut guess = String::new();
+        // Line input reading
         io::stdin()
             .read_line(&mut guess)
-            .expect("Impossible to read the line");
-
+            .expect("Impossible to read the line ");
+        // Shadowing
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Small"),
+        // Comparaison between values
+        match guess.cmp(&password) {
+            // Equal case
             Ordering::Equal => {
-                println!("You guessed the right number");
+                println!("The Password is correct");
                 break;
             }
-            Ordering::Greater => println!("Greater"),
+            Ordering::Less => println!("Password charachters are less"),
+            Ordering::Greater => println!("Password characters are more"),
         }
     }
 }
--- src/trash.rs
+++ src/trash.rs
@@ -1,22 +1,31 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-fn trash() {
+fn main() {
     println!("Guess the number");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Password is {}", secret_number);
 
-    println!("Input the Password");
-    let mut pass = String::new();
-    io::stdin()
-        .read_line(&mut pass)
-        .expect("Can't crack the Password");
+    let secret_number = rand::thread_rng().gen_range(0, 101);
+    println!("the secret number is {}", secret_number);
 
-    let pass: u32 = pass.trim().parse().expect("Please type a number");
+    loop {
+        println!("Enter the number");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Impossible to read the line");
 
-    match pass.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Bigger"),
-        Ordering::Equal => println!("Equal"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Small"),
+            Ordering::Equal => {
+                println!("You guessed the right number");
+                break;
+            }
+            Ordering::Greater => println!("Greater"),
+        }
     }
 }
+
