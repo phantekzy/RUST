4c36ce5792ee3bf0e5bfb01b7c618c3e42dd1467
phantekzy <mainilotfi@gmail.com>
2026-06-19 12:11:11 +0100

    DATA TYPES

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
 fn main() {
-    let mut x = 67;
-    println!("The value of x is : {}", x);
-    x = 5;
-    println!("The value of x is :{}", x);
-    const MAX_POINTS: u32 = 100_000;
-    println!("{}", MAX_POINTS);
-    // SHADOWING
-    let x = x + 1;
-    let x = x * 2;
-    println!("{}", x);
-
-    let spaces = "   ";
-    let spaces = spaces.len();
-    println!("{}", spaces);
+    // DATA TYPES
+    let guess: u32 = "67".parse().expect("not a number");
 }
