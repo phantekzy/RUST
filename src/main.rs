484db1a4154fe44cf2e4074925ec7b5ac07e1f05
phantekzy <mainilotfi@gmail.com>
2026-06-19 11:48:25 +0100

    assigning a variable here will cause data type error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
     let mut x = 67;
     println!("The value of x is : {}", x);
-    x = 69;
+    x = 5;
     println!("The value of x is :{}", x);
     const MAX_POINTS: u32 = 100_000;
     println!("{}", MAX_POINTS);
+    // SHADOWING
     let x = x + 1;
     let x = x * 2;
-    println!("Shadowing the x : {}", x);
+
+    let mut spaces = "   ";
+    spaces = spaces.len();
+    println!("{}", spaces);
 }
