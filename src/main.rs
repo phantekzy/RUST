9037c224d63528cb6e3f5d5ae57f8715f7f50d7f
phantekzy <mainilotfi@gmail.com>
2026-06-22 17:39:02 +0100

    we can do the destructuration by usig the periode follwing by the index of the tuple

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::fmt::Display;
 fn main() {
     // Tuples
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("the value of y is {}", y);
-    println!("the value of x is {}", x);
-    println!("the value of z is {}", z);
+    // Distructuring
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let first = x.0;
+    println!("the first value of x tuple is {}", first);
 }
