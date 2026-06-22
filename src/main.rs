25d7834f2bd8d1445cbb69ec839263e5f08ffd0a
phantekzy <mainilotfi@gmail.com>
2026-06-22 17:35:30 +0100

    distructuring the tuple values

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     let tup = (500, 6.4, 1);
     let (x, y, z) = tup;
     println!("the value of y is {}", y);
+    println!("the value of x is {}", x);
+    println!("the value of z is {}", z);
 }
