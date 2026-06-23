c860ca3c79ffb44705632d29796c9c8c0de6fb57
phantekzy <mainilotfi@gmail.com>
2026-06-23 11:06:56 +0100

    testing the auth

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,7 @@ fn main() {
     // Distructuring
     let x: (i32, f64, u8) = (500, 6.4, 1);
     let first = x.0;
+    let second = x.1;
     println!("the first value of x tuple is {}", first);
+    println!("the second value of x tuple is {}", second);
 }
