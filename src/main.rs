e44bdf8cbb5b3e72649710ee4c37c6a238185b82
phantekzy <mainilotfi@gmail.com>
2026-06-23 20:47:28 +0100

    Invalid Array element Access

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,10 @@
 fn main() {
     // The Array type
-    let _months = [
-        "janvier",
-        "fevrier",
-        "mars",
-        "avril",
-        "mai",
-        "june",
-        "july",
-        "August",
-        "September",
-        "Octobre",
-        "November",
-        "November",
-        "december",
-    ];
 
-    // The fist way
-    let _a: [i32; 5] = [1, 2, 3, 4, 5];
-    // the second way
-    let b = [3; 5];
+    // Invalid Array elements
+    let a = [0, 1, 2, 3, 4, 5];
+    let index = 10;
+    let element = a[index];
 
-    // Accessing an Array
-    let one = [1, 2, 3, 4, 5];
-    let first = one[0];
-    println!("first : {}", first);
-    let second = one[2];
-    println!("second : {}", second);
+    println!("The value of element is : {} ", element);
 }
