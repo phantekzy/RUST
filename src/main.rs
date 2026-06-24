7ae12760e8df0a00ced3e1156bdd871e07b34b3d
phantekzy <mainilotfi@gmail.com>
2026-06-24 15:18:55 +0100

    Expressions in Rust

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ fn main() {
     // Functions
     // Function parameters (Arguments)
     // Statments and Expressions in Function Bodies
-    let y = 6; // This is a statement
-    let x = (let y = 6); // Statements do not resturn values , and we cannot assign a let statement
-                         // to another variable
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is {}", y)
 }
