277fa9d1105987f40ba5ddc75bc93a7611b09e3f
phantekzy <mainilotfi@gmail.com>
2026-06-24 12:34:17 +0100

    statements and expressions in functions bodies

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,5 @@ fn main() {
     // Functions
     // Function parameters (Arguments)
     // Statments and Expressions in Function Bodies
-    another_function(5, 67);
-}
-
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is : {}", x);
-    println!("The value of y is : {}", y);
+    let y = 6;
 }
