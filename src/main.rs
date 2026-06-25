dc2020caea1dcf3fc0f125b6579de1f6087d3347
phantekzy <mainilotfi@gmail.com>
2026-06-25 11:58:16 +0100

    Functions with return values

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+// Functions
+// Functions with return values
+fn five() -> i32 {
+    5
+}
 fn main() {
-    // Functions
-    // Function parameters (Arguments)
-    // Statments and Expressions in Function Bodies
-    let x = 2;
-    let y = {
-        let x = 3;
-        x + 1
-    };
-    println!("The value of y is {}", y)
+    let x = five();
+    println!("The value of x is {}", x);
 }
