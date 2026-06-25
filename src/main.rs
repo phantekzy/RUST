e51e1046e9b781185137e56abeaa19a51fe52333
phantekzy <mainilotfi@gmail.com>
2026-06-25 21:14:32 +0100

    Control flow , If expressions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-// Functions
-// Functions with return values
+// Control flow
+// If Expressions
 fn main() {
-    let x = plus_one(5);
-    println!("The value of x is : {}", x);
-}
-
-fn plus_one(x: i32) -> i32 {
-    x + 1;
+    let number = 3;
+    if number > 5 {
+        println!("Condition was true");
+    } else {
+        println!("Condition was false");
+    }
 }
