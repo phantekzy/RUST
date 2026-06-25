c8f03b563cd80521f14f7e37344e0fe1b7318d52
phantekzy <mainilotfi@gmail.com>
2026-06-25 12:14:07 +0100

    Functions with return values

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // Functions
 // Functions with return values
-fn five() -> i32 {
-    5
-}
 fn main() {
-    let x = five();
-    println!("The value of x is {}", x);
+    let x = plus_one(5);
+    println!("The value of x is : {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
