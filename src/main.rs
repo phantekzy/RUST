3a15c56f3c51c85cb5712688b56f3477316a478a
phantekzy <mainilotfi@gmail.com>
2026-06-25 11:14:37 +0100

    Expressions in Rust

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     // Functions
     // Function parameters (Arguments)
     // Statments and Expressions in Function Bodies
+    let x = 2;
     let y = {
         let x = 3;
         x + 1
