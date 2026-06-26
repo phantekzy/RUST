b033dbeab972a1b1f277016177973c47578e59e8
phantekzy <mainilotfi@gmail.com>
2026-06-26 14:33:37 +0100

    each arm should have the same type

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // If Expressions
 // using if in a let statement
 fn main() {
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
+    let condition = false;
+    let number = if condition { 5 } else { "six" };
     println!("the value of number is {}", number);
 }
