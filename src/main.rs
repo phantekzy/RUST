c22a9f71e75467daa05046ae7cae938f731e7b19
phantekzy <mainilotfi@gmail.com>
2026-06-26 16:11:38 +0100

    Collection loop using for

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@
 // Looping Through a condition with for
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
-    while index != 5 {
-        println!("The value is {}", a[index]);
-        index += 1;
+    for x in a.iter() {
+        println!("the value is {}", x)
     }
 }
