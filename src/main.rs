af2ad83b86959f05f8a4e95f70b6a704047eea2a
phantekzy <mainilotfi@gmail.com>
2026-06-26 20:20:12 +0100

    Ownership rule : - Each value in Rust has a variable called Owner - There can be only one owner at the time

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     // - Each value in Rust has a variable that's called its owner.
     // The string RUST is created on the heap
     // The variable s is the official owner of that Data
-    let s = String::from("RUST");
+    let s1 = String::from("RUST"); // s1 OWNS the String
     // - There can be only one owner at a time.
+    let s2 = s1; // Ownership is moved from s1 to s2
     // - When the owner goes out of scope , the value will be dropped.
+    println!("{}", s2);
 }
