31362c952d871d6bfd708c8709e0be207a35bf0e
phantekzy <mainilotfi@gmail.com>
2026-06-26 20:06:56 +0100

    Ownership rule : - Each value in Rust has a variable called Owner

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 //THE STACK AND THE HEAP
-
 // OWNERSHIP
 // OWNERSHIP RULES
-// - Each value in Rust has a variable that's called its owner.
-// - There can be only one owner at a time.
-// - When the owner goes out of scope , the value will be dropped.
-fn main() {}
+fn main() {
+    // - Each value in Rust has a variable that's called its owner.
+    // The string RUST is created on the heap
+    // The variable s is the official owner of that Data
+    let s = String::from("RUST");
+    // - There can be only one owner at a time.
+    // - When the owner goes out of scope , the value will be dropped.
+}
