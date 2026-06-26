44b6467cc4356ebce749eef3fed7f17dbb846fbe
phantekzy <mainilotfi@gmail.com>
2026-06-26 14:29:39 +0100

    if condition in let

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 // Control flow
 // If Expressions
+// using if in a let statement
 fn main() {
-    let number = 6;
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisble by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by the numbers 4,3 and 2");
-    }
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("the value of number is {}", number);
 }
