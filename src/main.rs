9c7f95f92f434d64f1cca9eddff6c671b38049a9
phantekzy <mainilotfi@gmail.com>
2026-06-26 14:26:19 +0100

    else if not if

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ fn main() {
     let number = 6;
     if number % 4 == 0 {
         println!("number is divisible by 4");
-    }
-    if number % 3 == 0 {
+    } else if number % 3 == 0 {
         println!("number is divisble by 3");
-    }
-    if number % 2 == 0 {
+    } else if number % 2 == 0 {
         println!("number is divisible by 2");
     } else {
         println!("number is not divisible by the numbers 4,3 and 2");
