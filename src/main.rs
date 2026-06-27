022756dfc3e53e45ad40c36ade3eb3e37e993816
phantekzy <mainilotfi@gmail.com>
2026-06-27 21:12:34 +0100

    Literals vs Strings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 //THE STACK AND THE HEAP
 // OWNERSHIP
 // String type
-fn main() {}
+// Why strings can be mutated but not litterals ?
+fn main() {
+    let mut s = String::from("hello");
+    s.push_str(", phantekzy");
+    println!("{}", s);
+}
