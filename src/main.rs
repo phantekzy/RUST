5f1d4b335f6cf75335a7da504da4ad667ce0e879
phantekzy <mainilotfi@gmail.com>
2026-06-28 13:58:29 +0100

    String litteral

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@
 // Why strings can be mutated but not litterals ?
 fn main() {
     let mut s = String::from("hello");
-    s.push_str(", phantekzy");
-    println!("{}", s);
+    s.push_str(", phantekzy"); // push_str appends a litteral to a String
+    println!("{}", s); // This will print 'phantekzy'
 }
