d2d688a5827f8bedddf321bb2d7eed78410e5162
phantekzy <mainilotfi@gmail.com>
2026-06-28 21:20:28 +0100

    String Data and how rust copy it

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,16 @@
 // Memory and Allocation
 // Ways That Variables and Data Interact : Move
 fn main() {
+    // Integers
     let x = 5;
     let y = x;
-    println!("{}", y)
+    println!("{}", y);
     // Integers have a fixed size that is known in compile time , they live in stack
     // Rust give integers a special property called Copy trait
+    // Strings
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}", s2);
+    // When we assign s1 to s2 , the String Data is copied , meaning we copy
+    // the pointer , the length and the capacity that are on the stack.
 }
