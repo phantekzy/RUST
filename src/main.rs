3e7e57521df442c889bd6a8933ba74280c9f0f32
phantekzy <mainilotfi@gmail.com>
2026-06-28 20:47:22 +0100

    Copy treat

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@
 // String type
 // Why strings can be mutated but not litterals ?
 // Memory and Allocation
+// Ways That Variables and Data Interact : Move
 fn main() {
-    let mut s = String::from("hello");
+    let x = 5;
+    let y = x;
+    println!("{}", y)
+    // Integers have a fixed size that is known in compile time , they live in stack
+    // Rust give integers a special property called Copy trait
 }
-// the space taken by the variable s is automaticly returned the ths OS
-// After closing the scope
