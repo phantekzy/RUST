61625eff59b6c086d19c6203711e8028f0ce9e2f
phantekzy <mainilotfi@gmail.com>
2026-06-29 13:49:28 +0100

    The clone function in Rust

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,7 @@ fn main() {
     let x = 5;
     let y = x;
     println!("{}", y);
-    // Integers have a fixed size that is known in compile time , they live in stack
-    // Rust give integers a special property called Copy trait
-    // Strings
     let s1 = String::from("hello");
-    let s2 = s1; // The ownershp moves from s1 to s2 
-    println!("{} world", s1); // When we free Memory it frees only on s2 because it is the only
-    // owner right now
-    // Rust in this case do not call this a Shallow copy , it calls it Move
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
