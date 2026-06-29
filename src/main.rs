007702b5fc52baa52ca0ec383b6432747e381202
phantekzy <mainilotfi@gmail.com>
2026-06-29 15:36:35 +0100

    Difference between Strings and Integers

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,8 @@ fn main() {
     let x = 5;
     let y = x;
     println!("x = {}, y = {}", x, y);
+    // Difference between Integers and Strings
+    let s1 = String::from("hi");
+    let s2 = s1;
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
