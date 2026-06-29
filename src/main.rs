4755b9c8e806c0048acf11f7c91febb8990492a3
phantekzy <mainilotfi@gmail.com>
2026-06-29 15:16:11 +0100

    Stack only data : Copy

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 //THE STACK AND THE HEAP
 // OWNERSHIP
-// String type
-// Why strings can be mutated but not litterals ?
 // Memory and Allocation
-// Ways That Variables and Data Interact : Move
+// Stack Only Data
 fn main() {
     // Integers
     let x = 5;
     let y = x;
-    println!("{}", y);
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("s1 = {}, s2 = {}", s1, s2);
+    println!("x = {}, y = {}", x, y);
 }
