10350f3163e17f49531ffbb972f64e527fa6896e
phantekzy <mainilotfi@gmail.com>
2026-06-30 16:47:03 +0100

    Ownership and functions

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ fn main() {
 fn takes_ownership(some_string: String) {
     // some_string comes into scope
     println!("{}", some_string)
-}
+} // Here some_string goes out of scope and 'drop' function is called 
+// Memory is freed
 
 fn makes_copy(some_integer: i32) {
     // some_integer comes into scope
     println!("{}", some_integer)
-}
+} // Here some_integer goes out of scope . Nothing special happens
