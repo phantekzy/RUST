34be0a1f1efacecbbf4b60be27977826662faad3
phantekzy <mainilotfi@gmail.com>
2026-06-30 16:45:47 +0100

    Ownership and functions

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,13 @@
 fn main() {
     let s = String::from("FUNCTIONS");
 }
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string)
+}
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer)
+}
