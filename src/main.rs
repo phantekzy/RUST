b5685357b9aba6d1e9cf32f51a37007b671c1705
phantekzy <mainilotfi@gmail.com>
2026-06-30 18:03:13 +0100

    Return values and scope

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@
 fn main() {}
 
 fn gives_ownership() -> String {
+    // gives_ownership will move
+    // its return value into the
+    // function that calls it
     let some_string = String::from("String");
-    some_string // Returning the variable
+    // some_string comes into scope
+    some_string
+    // some_string is returned and moves out to the
+    // calling function
 }
