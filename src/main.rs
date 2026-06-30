6bd28fb26f8885915b291e481c8a05ac55145759
phantekzy <mainilotfi@gmail.com>
2026-06-30 16:42:41 +0100

    Ownership and functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 //THE STACK AND THE HEAP
 // OWNERSHIP
-// Memory and Allocation
-// Stack Only Data
+// OWNERSHIP AND FUNCTIONS
 fn main() {
-    // Integers
-    let x = 5;
-    let y = x;
-    println!("x = {}, y = {}", x, y);
-    // Difference between Integers and Strings
-    let s1 = String::from("hi");
-    let s2 = s1;
-    println!("s1 = {}, s2 = {}", s1, s2);
+    let s = String::from("FUNCTIONS");
 }
