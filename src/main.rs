749faf33e490a46512d371dfdf3d763404c2bdd0
phantekzy <mainilotfi@gmail.com>
2026-07-01 16:16:08 +0100

    References and Borrwing

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 // OWNERSHIP
 // REFRENCES AND BORROWING
 fn main() {
-    let s1 = String::from("REFERENCES");
-    let len = calculate_length(&s1);
+    let s1 = String::from("REFERENCES"); // s1 enters to the scope
+    let len = calculate_length(&s1); // A reference is like a pointer in C
     println!("The length of '{}' is {}", s1, len)
 }
 
 fn calculate_length(s: &String) -> usize {
+    // the function takes a String reference
     s.len()
 }
