d5e80f0fdda2f093c20c44703c06c20288371eba
phantekzy <mainilotfi@gmail.com>
2026-07-01 15:55:24 +0100

    References and Borrwing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 // REFRENCES AND BORROWING
 fn main() {
     let s1 = String::from("REFERENCES");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}", s1, len)
 }
 
-fn calculate_length(s: &String) -> usize {}
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
