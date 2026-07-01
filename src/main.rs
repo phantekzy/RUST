725ef337456309ac4be585ea3106005c39d50882
phantekzy <mainilotfi@gmail.com>
2026-07-01 17:48:31 +0100

    References & in Rust , and the scope

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 fn main() {
     let s1 = String::from("REFERENCES"); // s1 enters to the scope
     let len = calculate_length(&s1); // A reference is like a pointer in C
-    println!("The length of '{}' is {}", s1, len)
 }
 
 fn calculate_length(s: &String) -> usize {
-    // the function takes a String reference
+    // s is a reference to a string
     s.len()
-}
+} // here , s goes out of the scope , but because it does not have
+// ownership of it refers to , so nothing happens
