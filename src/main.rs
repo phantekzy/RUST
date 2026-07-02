9a3129f1eb5f18dc9b3492227b62a7a460684d75
phantekzy <mainilotfi@gmail.com>
2026-07-02 15:36:52 +0100

    Borrowing and modifying borrowed values

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@
 // OWNERSHIP
 // REFRENCES AND BORROWING
 fn main() {
-    let s1 = String::from("REFERENCES"); // s1 enters to the scope
-    let len = calculate_length(&s1); // A reference is like a pointer in C
-    println!("The length of '{}' is {}", s1, len);
+    let s = String::from("BORROWING");
+    change(&s);
 }
-fn calculate_length(s: &String) -> usize {
-    // s is a reference to a string
-    s.len()
+
+fn change(some_string: &String) {
+    some_string.push_str("a value");
 }
-// here , s goes out of the scope , but because it does not have
-// ownership of it refers to , so nothing happens
