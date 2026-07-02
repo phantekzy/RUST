7c0080bd8afb9f2c4e1b2ffcb3ee67e33eafde33
phantekzy <mainilotfi@gmail.com>
2026-07-02 17:12:22 +0100

    References has a rule : You can have only one mutable reference to a particular piece of data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 //THE STACK AND THE HEAP
 // OWNERSHIP
 // REFRENCES AND BORROWING
+// MUTABLE REFRENCES
 fn main() {
-    let s = String::from("BORROWING");
-    change(&s);
-}
-
-fn change(some_string: &String) {
-    some_string.push_str("a value");
+    let mut s = String::from("BORROWING");
+    let mut r = &mut s;
+    // We can have only one mutable reference
+    let mut r1 = &mut s;
+    println!("{}, {}", r, r1);
 }
