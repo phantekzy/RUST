c9f7effdfae163a4f6af57c661b784ad376f51ba
phantekzy <mainilotfi@gmail.com>
2026-07-03 18:41:22 +0100

    Dangling References

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 //THE STACK AND THE HEAP
 // OWNERSHIP
 // REFRENCES AND BORROWING
-// MUTABLE REFRENCES
+// DANGLING REFRENCES
 fn main() {
-    let mut s = String::from("BORROWING");
-    // New scope
-    {
-        let r = &mut s;
-        println!("{} ", r);
-    };
-    // r goes out of the scope , and here we can make a new reference with no problems
-    // A similar rule exists for combining mutable and immutable references
-    // Cannot have an mutable reference if we have immutable ones.
-    let r1 = &s; // No problem
-    let r2 = &s; // No problem
-    let r3 = &mut s; // ERROR
+    let reference_to_nothing = dangle();
+}
 
-    println!("{},{} and {}", r1, r2, r3);
+fn dangle() -> &String {
+    let s = String::from("hello");
+    &s
 }
