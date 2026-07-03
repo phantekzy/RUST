eda557c017c4e15bebbd9a3fc4414ef4d2ce5276
phantekzy <mainilotfi@gmail.com>
2026-07-03 21:30:45 +0100

    Dangling References

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@
 fn main() {
     let reference_to_nothing = dangle();
 }
-
-fn dangle() -> &String {
+// The sollution is to return the String directly and move Ownership
+fn dangle() -> String {
     // dangle returns a reference to a String
     let s = String::from("hello");
-    &s // we return a reference to a String
+    s // we return a reference to a String
 } // s goes out of the scope , and is dropped . it's memory goes away
