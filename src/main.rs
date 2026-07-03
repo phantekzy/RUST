7b9a8af152f542ecedcb657b65972ba49d820bb1
phantekzy <mainilotfi@gmail.com>
2026-07-03 11:54:26 +0100

    References has a rule : You can have only one mutable reference to a particular piece of data

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 // MUTABLE REFRENCES
 fn main() {
     let mut s = String::from("BORROWING");
-    let mut r = &mut s;
+    let r = &mut s;
     // We can have only one mutable reference
     let mut r1 = &mut s;
     println!("{}, {}", r, r1);
