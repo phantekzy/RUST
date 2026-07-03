f25ad44e6ee770947d9608141f86c2c287262a0e
phantekzy <mainilotfi@gmail.com>
2026-07-03 12:03:28 +0100

    References has a rule : You can have only one mutable reference to a particular piece of data

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let mut s = String::from("BORROWING");
     let r = &mut s;
     // We can have only one mutable reference
-    let r1 = &mut s;
-    println!("{}, {}", r, r1);
+    // The benefit of having this restriction is that Rust can prevent
+    // data races at compile time .
+    println!("{} ", r);
 }
