87e959795a53ed9eb8db3c5e262d5b3842cfa9b2
phantekzy <mainilotfi@gmail.com>
2026-07-04 11:08:21 +0100

    Slice type - i have to review loops in Rust , because it is diffrente here

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@
 // SLICE TYPE
 fn main() {
     // I HAD TO REVIEW LOOPS
+    // Consuming the data
     let names = vec!["Lotfi", "Yuro", "King"];
-    for name in &names {
+    for name in names {
         println!("{}", name);
     }
+    // ERROR
     println!("Total of people : {}", names.len());
 }
