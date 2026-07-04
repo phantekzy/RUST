51448217aadfe19bb8b213c6be16980c05951d68
phantekzy <mainilotfi@gmail.com>
2026-07-04 11:02:26 +0100

    Slice type - i have to review loops in Rust , because it is diffrente here

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@
 // OWNERSHIP
 // REFRENCES AND BORROWING
 // DANGLING REFRENCES
+// SLICE TYPE
 fn main() {
-    let reference_to_nothing = dangle();
-    println!("{}", reference_to_nothing);
+    // I HAD TO REVIEW LOOPS
+    let names = vec!["Lotfi", "Yuro", "King"];
+    for name in &names {
+        println!("{}", name);
+    }
+    println!("Total of people : {}", names.len());
 }
-// The sollution is to return the String directly and move Ownership
-fn dangle() -> String {
-    // dangle returns a reference to a String
-    let s = String::from("hello");
-    s // we return a reference to a String
-} // s goes out of the scope , and is dropped . it's memory goes away
