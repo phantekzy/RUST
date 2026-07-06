ac5a5011a8a725bdf6b75f06b6d99111e6368b16
phantekzy <mainilotfi@gmail.com>
2026-07-06 16:32:20 +0100

    String slices

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,10 @@
 // REFRENCES AND BORROWING
 // DANGLING REFRENCES
 // SLICE TYPE
+// STRING SLICES
 fn main() {
-    let mut s = String::from("Hello World");
-    let _word = first_word(&s);
+    let s = String::from("Maini Lotfi");
+    let maini = &s[0..5];
 
-    s.clear(); // Clear empties the String 
-} // word still has the value 5 but there is no more strings 
-// that we can use the value 5 with . word is totaly now invalid
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-    s.len()
+    println!("{}", maini);
 }
