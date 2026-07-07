c4d266e87194f6695fd8b3a6660a1a293add428d
phantekzy <mainilotfi@gmail.com>
2026-07-07 22:40:23 +0100

    String slices

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@
 fn main() {
     // Variables
     let s = String::from("Maini Lotfi");
+    let p = String::from("Testing");
     // String slices
     let maini = &s[0..5];
     let lotfi = &s[6..11];
+    let test = &p[0..7];
     // Print
     println!("{}", maini);
     println!("{}", lotfi);
+    println!("{}", test);
 }
