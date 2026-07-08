2e9ec5175412b74fc53d02064caedb5d3f4cc98e
phantekzy <mainilotfi@gmail.com>
2026-07-08 11:29:36 +0100

    String slices

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@
 // STRING SLICES
 fn main() {
     let s = String::from("Hello");
-    // If we want to start at the first index , we can drop the value before two periodes
-    // Both are equqls
-    let slice = &s[0..2];
-    let slice = &s[..2];
+    let len = s.len();
+    let slice = &s[3..len];
+    // We can drop the trailing number to get the last byte
+    let slice = &s[3..];
 }
