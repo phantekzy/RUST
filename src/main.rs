0117e0c5c4d057292008d5c9299aeb7387628842
phantekzy <mainilotfi@gmail.com>
2026-07-10 16:13:10 +0100

    string literals are slices

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,6 @@
 // STRING SLICES
 // STRING SLICES ARE SLICES
 fn main() {
-    let mut s = String::from("Hello world");
-    let word = first_word(&s);
-    s.clear();
-    println!("The first word is {}", word)
+    let s = "Hello,world";
+    println!("{}", s);
 }
-
-// first_word function should be written like this
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
-    }
-    &s[..]
-}
-
-// Second word function should be written like this
-fn second_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[i..];
-        }
-    }
-    &s[..]
-}
-
-// CONTINUE From STRING LITERALS ARE SLICES
