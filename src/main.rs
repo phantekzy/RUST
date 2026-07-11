f1b955b01a229d737da2f2954fc6037ec3a5de8b
phantekzy <mainilotfi@gmail.com>
2026-07-11 18:06:33 +0100

    i have to review this part fml

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
-use std::usize;
-
 //THE STACK AND THE HEAP
 // OWNERSHIP
 // REFRENCES AND BORROWING
 // DANGLING REFRENCES
 // SLICE TYPE
 // STRING SLICES
-// STRING SLICES ARE SLICES
 fn main() {
-    let mut s = String::from("hello word");
-    let word = first_word(&s);
-    s.clear();
-    println!("{}", word)
-}
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-    s.len()
+    let s = String::from("Hello world");
+    let hello = &s[..5];
+    println!("{}", hello)
 }
