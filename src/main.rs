1390f4af8e84f1a4546e84bc4e675a39ca2e5fac
phantekzy <mainilotfi@gmail.com>
2026-07-12 17:36:20 +0100

    made the first word function

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,13 @@ fn main() {
     println!("{}", world);
     println!("{}", all);
 }
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
