41c6dce5fa2bec5073abcded268e1fd58a6329db
phantekzy <mainilotfi@gmail.com>
2026-07-12 11:29:51 +0100

    i have to review this part fml

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ fn main() {
     // The slice data structure , stores the starting position
     // and the length of the slice
     let hello = &s[..5];
-    let world = &s[6..11];
+    let world = &s[6..];
+    // To take the entire string
+    let all = &s[..];
     println!("{}", hello);
     println!("{}", world);
+    println!("{}", all);
 }
