507ce6f05ccc4640eceb9d60b4fe138a64a05a7c
phantekzy <mainilotfi@gmail.com>
2026-07-18 12:27:25 +0100

    let us move the structs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-//THE STACK AND THE HEAP
-// BORROWING AND OWNERSHIP IN RUST
-// SLICE TYPE
-// STRING SLICES
-// STRING lITERALS ARE SLICES
-//OTHER SLICES
+// STRUCTS
+
 // MAIN FUNCTION
-fn main() {
-    let a = [1, 2, 3, 4, 5];
-    let slice = &a[1..3]; // The slice has the type &[i32]
-}
+fn main() {}
