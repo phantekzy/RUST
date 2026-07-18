c0e1b4bc5a01b9b2eb5a5c29ae8b99f9c1d8986f
phantekzy <mainilotfi@gmail.com>
2026-07-18 12:53:10 +0100

    Using the Struct

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ struct User {
     sign_in_count: u64, // the curly brackets "fields"
     active: bool,       //
 }
-
+// How to use the Struct
 // MAIN FUNCTION
-fn main() {}
+fn main() {
+    let user1 = User {
+        email: String::from("xxx@mail.com"),
+        username: String::from("xxx69"),
+        active: true,
+        sign_in_count: 1,
+    };
+}
