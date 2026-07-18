49d4a702b7c45317d9ce90b4ec21432cb3f5135b
phantekzy <mainilotfi@gmail.com>
2026-07-18 11:45:36 +0100

    let us refere to a part of an Array

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,9 @@
 // SLICE TYPE
 // STRING SLICES
 // STRING lITERALS ARE SLICES
-
+//OTHER SLICES
 // MAIN FUNCTION
 fn main() {
-    let my_string = String::from("Hello World");
-    // First word works on slices of strings
-    let word = first_word(&my_string[..]);
-
-    let my_string_literal = "hello world";
-    // First word works on slices of string literals
-    let word = first_word(my_string_literal);
-
-    // Because String literals are String slices already ,
-    // this works too , without the slice syntax !
-    let word = first_word(my_string_literal);
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
 }
-
-// FIRST WORD FUNCTION
-// String slices as parameters
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
-    }
-    &s[..]
-}
-
-// SECOND WORD FUNCTION
-/*
-fn second_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[i..];
-        }
-    }
-    &s[..]
-}
-*/
