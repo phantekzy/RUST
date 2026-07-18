ebb77c11174d550fc93474eed4263e8314f0f494
phantekzy <mainilotfi@gmail.com>
2026-07-18 12:34:21 +0100

    How to create a struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 // STRUCTS
+// Defining and Instantiating Structs
+
+// PS :
+// To define a struct we enter the keyword Struct
+// and name the entire struct
+struct User {
+    username: String,   //
+    email: String,      // We call the data inside
+    sign_in_count: u64, // the curly brackets "fields"
+    active: bool,       //
+}
 
 // MAIN FUNCTION
 fn main() {}
