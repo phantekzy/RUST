96c037bbccc2a761a51a420ba787f1e1104f0ae0
phantekzy <mainilotfi@gmail.com>
2026-07-20 13:35:16 +0100

    Creating a function that returns a User instance with the given email and username

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@
 // and name the entire struct
 
 struct User {
-    username: String,   //
+    username: String,   // String
     email: String,      // We call the data inside
-    sign_in_count: u64, // the curly brackets "fields"
-    active: bool,       //
+    sign_in_count: u64, // The curly brackets "fields"
+    active: bool,       // Boolean
 }
 
 // How to use the Struct
@@ -17,11 +17,22 @@ struct User {
 fn main() {
     // Making the Struct mutable
     // You cannot change the values if the entire instance is not mutable
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("xxx@mail.com"),
         username: String::from("xxx69"),
         active: true,
         sign_in_count: 1,
     };
     user1.username = String::from("Phantekzy");
+}
+
+// Creating a build_user function that returns a User instance
+// with the given email and password
+fn build_user(email: String, username: String) -> User {
+    User {
+        username: username,
+        email: email,
+        sign_in_count: 1,
+        active: true,
+    }
 }
