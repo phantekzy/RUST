8f4b4be32c3ac150cfba6893cfb840ecc8463fe6
phantekzy <mainilotfi@gmail.com>
2026-07-22 14:13:10 +0100

    Another version of the program using Tuple struct  this time

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 // A Program, Using Struct
+// Refactoring with Tuples
 
 // Main Function
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
-    // Calculating the Area using the function bellow
-    println!(
-        "The Area of the rectangle is {} square pixels.",
-        area(width1, height1)
-    )
+    let rect1 = (30, 50);
+    println!("The area of the rectangle is {} square pixels", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
