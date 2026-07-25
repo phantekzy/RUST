369c04971da61e01f2b3284d5f047820b2ae1354
phantekzy <mainilotfi@gmail.com>
2026-07-25 16:06:19 +0100

    Using the unwriten method

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,7 @@ fn main() {
         width: 60,
         height: 45,
     };
+    // Using the as-yet-unwritten can hold method
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
