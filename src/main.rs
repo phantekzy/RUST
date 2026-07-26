11537f8df540298f228a8f67d9b5bdba16f037c3
phantekzy <mainilotfi@gmail.com>
2026-07-26 18:15:08 +0100

    Calling the function with either variant

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,10 @@ fn main() {
     // We use the double colon to separate to call the values and separate the two
     let v4 = IpAddrKind::V4;
     let v6 = IpAddrKind::V6;
-
-    // IpAddrKind::v4 and IpAddrKind::v6 are of the same type : IpAddrKind
-    // We can then , for instance , define Functions that takes any IpAddrKind
-    fn route(ip_kind: IpAddrKind) {}
+    // And we can call this function with either variant
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
+// IpAddrKind::v4 and IpAddrKind::v6 are of the same type : IpAddrKind
+// We can then , for instance , define Functions that takes any IpAddrKind
+fn route(ip_kind: IpAddrKind) {}
