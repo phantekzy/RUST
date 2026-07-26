4c7cb723f3513403dfd5d08619c3102b044888c3
phantekzy <mainilotfi@gmail.com>
2026-07-26 18:13:35 +0100

    v4 and v5 are the same type

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,8 @@ fn main() {
     // We use the double colon to separate to call the values and separate the two
     let v4 = IpAddrKind::V4;
     let v6 = IpAddrKind::V6;
+
+    // IpAddrKind::v4 and IpAddrKind::v6 are of the same type : IpAddrKind
+    // We can then , for instance , define Functions that takes any IpAddrKind
+    fn route(ip_kind: IpAddrKind) {}
 }
