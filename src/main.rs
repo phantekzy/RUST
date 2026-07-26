702f079ec7afd6a3ee1509c000d8c075b54ae222
phantekzy <mainilotfi@gmail.com>
2026-07-26 17:29:27 +0100

    Enumerations : Calling values

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 // Enumerations
 // Enums allows us to define a type by enumerating its possible values.
+// Defining an enum
+
+// IP ADRESS ENUM
+enum IpAddrKind {
+    // IpAddrKind is now a costum data TYpe
+    V4,
+    V6,
+}
 
 // Main Function
-fn main() {}
+fn main() {
+    // Enum Values
+    // We use the double colon to separate to call the values and separate the two
+    let v4 = IpAddrKind::V4;
+    let v6 = IpAddrKind::V6;
+}
