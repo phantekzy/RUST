8d11e1a3d12f6db70acba8017fbbbf543c9b6469
phantekzy <mainilotfi@gmail.com>
2026-07-28 18:17:53 +0100

    Enums values

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,13 @@ enum IpAddKind {
     V4,
     V6,
 } // IpAddKind is now a costum Data type we can use it elsewhere in our code 
-// Storing the data in Struct ipAddr
-// Struts are AND
-struct IpAddr {
-    kind: IpAddKind,
-    address: String,
+
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
-// Enum values
 // Main function
 fn main() {
-    let four = IpAddKind::V4;
-    let six = IpAddKind::V6;
-    // We can call the function with either variants
-    route(IpAddKind::V4);
-    route(IpAddKind::V6);
-    // Calling the struct
-    // Home
-    let home = IpAddr {
-        kind: IpAddKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    // Loopback
-    let Loopback = IpAddr {
-        kind: IpAddKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopbacl = IpAddr::V6(String::from("::1"));
 }
-
-// We can then,for instance , define a function that takes any
-// IpAddKind
-fn route(ip_kind: IpAddKind) {}
