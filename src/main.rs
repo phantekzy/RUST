bfcc431462daa55d967d8433d7666ed3ca67e9bc
phantekzy <mainilotfi@gmail.com>
2026-07-28 18:23:03 +0100

    Enums values

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ enum IpAddKind {
     V4,
     V6,
 } // IpAddKind is now a costum Data type we can use it elsewhere in our code 
-
+#[derive(Debug)]
 enum IpAddr {
     V4(String),
     V6(String),
@@ -17,4 +17,5 @@ enum IpAddr {
 fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
     let loopbacl = IpAddr::V6(String::from("::1"));
+    println!("Home : {:?}", home);
 }
