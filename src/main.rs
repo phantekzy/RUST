b3a76957b57f56127f0078a34188ea1722070fab
phantekzy <mainilotfi@gmail.com>
2026-07-28 15:32:51 +0100

    Enums

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 enum IpAddKind {
     V4,
     V6,
-}
+} // IpAddKind is now a costum Data type we can use it elsewhere in our code 
+// Enum values
 // Main function
-fn main() {}
+fn main() {
+    let four = IpAddKind::V4;
+    let six = IpAddKind::V6;
+}
