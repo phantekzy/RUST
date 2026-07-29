c99040eb8e418ec6402dc790ec6323c8c9ae7d7e
phantekzy <mainilotfi@gmail.com>
2026-07-29 14:09:15 +0100

    Standard Libray and the IpAddr

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,16 @@
 //
 // There is another advantage to using an enum rather than Struct
 // Each variant can have different type and amounts of associated data .
-enum _IpAddKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
-} // IpAddKind is now a costum Data type we can use it elsewhere in our code 
-#[derive(Debug)]
-enum IpAddr {
-    V4(String),
-    V6(String),
+
+// How the Standard Libray Defines IpAddr::
+// IP version 4 Address
+struct Ipv4Addr {
+    // -- snip --
 }
+// IP version 4 Address
+struct Ipv6Addr {
+    // -- snip --
+}
+
 // Main function
-fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
-    let loopbacl = IpAddr::V6(String::from("::1"));
-    println!("Home : {:?}", home);
-}
+fn main() {}
