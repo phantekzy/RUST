f2e76a8bdb6a7360ac6de7460e4be452a006e5a6
phantekzy <mainilotfi@gmail.com>
2026-07-30 10:26:54 +0100

    Defining User Struct

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
-// ENUM
-// Storing the data in Structs
-// Enums are OR
-//
-// There is another advantage to using an enum rather than Struct
-// Each variant can have different type and amounts of associated data .
-
-// How the Standard Libray Defines IpAddr::
-// IP version 4 Address
-// Standard Libray type are growable data
-enum IpAddrKind {
-    V4,
-    V6,
+// USING STRUCTS TO STRUCTURE RELATED DATA
+// Defining and Instantiating Structs
+// User Struct
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
-struct Ipv4Addr {
-    // -- snip --
-}
-// IP version 4 Address
-struct Ipv6Addr {
-    // -- snip --
-}
-enum IpAddr {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr),
-}
-
-// Main function
-fn main() {
-    // Creating instandces of each variant of IpAddrKind using the ":"
-    // Both values are of the same type IpAddrKind
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    // We can call  the route function with either variants
-    // Calling the V4 variant
-    route(IpAddrKind::V4);
-    // Calling the V6 variant
-    route(IpAddrKind::V6);
-}
-
-// We can for instance , create a function that takes any IpAddrKind type
-fn route(ip_kind: IpAddrKind) {}
+fn main() {}
