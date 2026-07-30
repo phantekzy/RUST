936ccc597bf745666840714bcbb0a47e02688c93
phantekzy <mainilotfi@gmail.com>
2026-07-30 10:32:06 +0100

    The Struct Blueprint vs Instance

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@
 // Defining and Instantiating Structs
 // User Struct Definition
 // Structs are like Objects in Languages like Javascript
+// The Blueprint
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
 }
-fn main() {}
+fn main() {
+    // Using a Struct
+    // We create an Instance of that struct
+    // The Instance
+    let user1 = User {
+        username: String::from("Phantekzy"),
+        email: String::from("Phantekzy@gmail.com"),
+        sign_in_count: 3,
+        active: false,
+    };
+}
