7b073d8d4cb55b841c9b88244511a6282320ad28
phantekzy <mainilotfi@gmail.com>
2026-08-06 14:32:18 +0100

    Creating the Struct

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@
 // Unit-like Structs without Any Fields
 //
 // Creating q Program using Structs
-// Main function
-fn main() {
-    // Rectangle
-    let rect1 = (30, 50);
-    // Printing the Area
-    println!("The Area of the rectangle is {} square pixels", area(rect1))
+// Rectangle Struct
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
+// Main function
+fn main() {}
 
 // Calculating the area of a rectangle specified by separate width and height variables
 // Refactoring with Structs
 // We add structs to add meaning by labeling the data
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
