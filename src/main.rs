5d44215c796efb8525d4c449891e0a6de2247a18
phantekzy <mainilotfi@gmail.com>
2026-08-07 16:35:21 +0100

    How to print while debuging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 // USING STRUCTS TO STRUCTURE RELATED DATA
-// Defining and Instantiating Structs
-// User Struct Definition
-// Structs are like Objects in Languages like Javascript
-// Using Tuple Structs Without Named Fields to create Different Types
-// The Blueprint
-// Unit-like Structs without Any Fields
-//
-// Creating q Program using Structs
+// Adding usefull functionalities with Derived Traits
+// Let's try to print the instance of rectangle while debugging
+
 // Rectangle Struct
 struct Rectangle {
     width: u32,
