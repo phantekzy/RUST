6403a1704e2dad60fe3644d03ace16342334231a
phantekzy <mainilotfi@gmail.com>
2026-08-07 16:35:49 +0100

    Cleaning

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,4 @@ struct Rectangle {
     height: u32,
 }
 // Main function
-fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    println!(
-        "The Area of the rectangle is {} square pixels.",
-        area(&rect1)
-    )
-}
-
-// The function takes the Rectangle Struct as parameter
-// It is an immutable borrow because its a read only
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+fn main() {}
