452ea5f34c4b2f43d9449f31e596b57414439920
phantekzy <mainilotfi@gmail.com>
2026-08-08 23:10:13 +0100

    Added the pretty formater

--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,5 @@ fn main() {
         height: 50,
     };
 
-    println!("rect1 is {}", rect1); //required by this formatting parameter
+    println!("rect1 is {:?}", rect1); //required by this formatting parameter
 }
