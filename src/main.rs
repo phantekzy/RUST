dceeddef0edc6bc87a0b29727e615adbca75bd66
phantekzy <mainilotfi@gmail.com>
2026-08-11 16:01:00 +0100

    Associated functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 // Method
-// Methods with more Parameters
-// Let us implement a second method on the Rectangle struct
-// This time , i want an instance of Rectangle to take another instance of Rectangle
-// and return true if the second Rectangle can fit completely within self
-// otherwise it should return false
+// Assocated Functions
 
 // Rectangle Struct
 struct Rectangle {
@@ -12,9 +8,11 @@ struct Rectangle {
 }
 impl Rectangle {
     // Added _ so the compiler shut the hell up
+    // Method
     fn _area(&self) -> u32 {
         self.width * self.height
     }
+    // Method
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
