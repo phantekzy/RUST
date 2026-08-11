e5918992930c169708cd8c5af4c9e6bdb655889c
phantekzy <mainilotfi@gmail.com>
2026-08-11 16:33:31 +0100

    Creating an Associated square function

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+    // Associated function
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 // Main function
