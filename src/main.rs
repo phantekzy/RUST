f8cf300a70063512e64f548022a9cc89744373e8
phantekzy <mainilotfi@gmail.com>
2026-08-11 15:51:53 +0100

    Implementing the can_hold method that takes another Rectangle instance as a parameter

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,14 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 
 // Main function
 fn main() {
