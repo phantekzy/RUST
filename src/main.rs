25110d19172f8440a7085d18fd58d8b7bf034d32
phantekzy <mainilotfi@gmail.com>
2026-08-11 12:25:14 +0100

    let me clear the code

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,6 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-// Implementation block
-impl Rectangle {
-    // Defining an area method on the Rectangle struct
-    fn area(&self) -> u32 {
-        self.height * self.width
-    }
-}
 
 // Main function
-fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    print!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    )
-}
+fn main() {}
