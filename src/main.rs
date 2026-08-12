7b76b4d49afe2ae733aa594dd6da42e1d9f42cc6
phantekzy <mainilotfi@gmail.com>
2026-08-12 12:10:19 +0100

    Enum Values

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,7 @@ fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 }
+
+// Note that the variants of the enums are namespaced under its indentifier.
+// We use a double colon to separate the two.
+// Both values IpAddrKind::V4 and IpAddrKind::V6 are of the same type : IpAddrKind.
