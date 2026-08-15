60e05df94569cb287751c71b6fd127736aea0227
phantekzy <mainilotfi@gmail.com>
2026-08-15 14:02:47 +0100

    Rewriting in a more concise way

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,7 @@ enum IpAddrKind {
     V6(String),
 }
 // Main function
-fn main() {}
+fn main() {
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
+}
