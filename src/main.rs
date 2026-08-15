e8209a85cf5d3c16beddbced847eece5142ce49c
phantekzy <mainilotfi@gmail.com>
2026-08-15 14:25:03 +0100

    Each variant cant have different types and amounts of associated data

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,10 @@
 // We can represent the same concept in a more concise way using an enum
 // PS : Enum -> OR  // Struct -> AND
 // Another advantage to using enums rather than structs :
-// Each variant can have different type and amounts of assiciated data
 enum IpAddrKind {
-    // IpAddrKind is now a custom data type
-    V4(String),
+    // Each variant can have different type and amounts of assiciated data
+    V4(u8, u8, u8),
     V6(String),
 }
 // Main function
-fn main() {
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
-    let loopback = IpAddrKind::V6(String::from("::1"));
-}
+fn main() {}
