5db4a3edea674a869d0cdc3253160372f48795ee
phantekzy <mainilotfi@gmail.com>
2026-08-15 17:04:19 +0100

    How standard libray defines IpAddr:

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 // CUSTOM TYPES
 // ENUMS AND PATTERN MATCHING
 // How the standard Library defines IpAddr:
-enum IpAddrKind {
-    // Each variant can have different type and amounts of assiciated data
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
+// It has the exact enum variants that we've defined and used
+// But it embeds the address data inside the variants in the form
+// of two different structs , which are defined differently for each variant :
+
+// IP version 4
+struct Ipv4Addr {}
+
+// IP version 6
+struct Ipv6Addr {}
 // Main function
 fn main() {
     let home = IpAddrKind::V4(127, 0, 0, 1);
