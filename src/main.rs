d5038d9f27a9bc0af218e815db39d8c3dab24555
phantekzy <mainilotfi@gmail.com>
2026-08-15 17:00:30 +0100

    How standard libray defines IpAddr:

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 // CUSTOM TYPES
 // ENUMS AND PATTERN MATCHING
-// Defining an Enum
-// Using enums has even more advantages
-// We do not have a way to store the actual IP adress data
-// We only know what kind it is
-// Enum
-// We can represent the same concept in a more concise way using an enum
-// PS : Enum -> OR  // Struct -> AND
-// Another advantage to using enums rather than structs :
+// How the standard Library defines IpAddr:
 enum IpAddrKind {
     // Each variant can have different type and amounts of assiciated data
     V4(u8, u8, u8, u8),
