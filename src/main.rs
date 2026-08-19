bbb05c6dae64e6b82fef1fc37dd66f3161240075
phantekzy <mainilotfi@gmail.com>
2026-08-19 22:25:07 +0100

    Another expmple of enums

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 // Another exemple of an enum in that has a wide
 // variety of types embedded in its variants
+
+// A Message enum whose variants each store different amounts and types values
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
