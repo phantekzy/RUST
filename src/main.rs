75e3a4c9c599f89faab9816e0b8ca4f4db0da53d
phantekzy <mainilotfi@gmail.com>
2026-08-19 22:03:47 +0100

    Another expmple of enums

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@
 
 // Another exemple of an enum in that has a wide
 // variety of types embedded in its variants
-
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 // Main function
 fn main() {}
