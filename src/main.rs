31f23e049bc25081472479f5b8620e70d89addb2
phantekzy <mainilotfi@gmail.com>
2026-08-23 21:55:36 +0100

    Defining an enum with vbariants such as the last example

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,8 @@ enum Message {
     Write(String),
     ChangeColor(i32, i32, i32),
 }
+// Defining an enum with variants such as the one in the upper enum
+// is similar to defining different kinds of struct definition , except the enum doesn't use
+// the struct keyword and all the variant s are grouped together under the Message type .
 // Main function
 fn main() {}
