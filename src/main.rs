55d4a5683c7604d50b828f66007cbe98b047b216
phantekzy <mainilotfi@gmail.com>
2026-08-23 21:51:53 +0100

    Another expmple of enums

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@
 // variety of types embedded in its variants
 
 // A Message enum whose variants each store different amounts and types values
+// This enum has four variants with different types :
+//  - Quit has no data associated with it at all.
+//  - Move includes an anonymouse struct inside it.
+//  - Write includes a single String.
+//  - ChangeColor includes three i32 values.
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
