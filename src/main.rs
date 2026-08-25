314ad6f42e8bb6c2e966519ae2bd287816f185f8
phantekzy <mainilotfi@gmail.com>
2026-08-25 08:14:23 +0100

    what is the Option enum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // CUSTOM TYPES
 // Standard Library Enum
 // Option enum
-
+// The Option Enum and its advantages over Null Values
+// The Option Type is used in many places because it encodes the very common scenario in which a
+// value can be something or it could be nothing
 // Main function
 fn main() {}
