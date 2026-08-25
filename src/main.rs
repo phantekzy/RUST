cbe890571b6423efbe51d968764a016c5dfd63c3
phantekzy <mainilotfi@gmail.com>
2026-08-25 08:18:42 +0100

    what is the Option enum

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@
 // The Option Enum and its advantages over Null Values
 // The Option Type is used in many places because it encodes the very common scenario in which a
 // value can be something or it could be nothing
+//
+// Expressing this concept in terms of the type system means the compiler can check
+// whether you've handled all the cases you should be handling;
+// this funcionality can prevent bugs that are extremely common in other programming languages
 // Main function
 fn main() {}
