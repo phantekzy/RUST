dc1efd382e3995dd50dd4fcc2ef54190a659d914
phantekzy <mainilotfi@gmail.com>
2026-09-04 19:38:28 +0100

    I have to review all this again

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 // CUSTOM TYPES
-// Standard Library Enum
-// Option enum
-// The Option Enum and its advantages over Null Values
-// The Option Type is used in many places because it encodes the very common scenario in which a
-// value can be something or it could be nothing
-//
-// Expressing this concept in terms of the type system means the compiler can check
-// whether you've handled all the cases you should be handling;
-// this funcionality can prevent bugs that are extremely common in other programming languages
-// Main function
-fn main() {}
+// Enum values
+// We can create instances of each of the two variants of IpAddrkind
+enum IpAddrkind {
+    V4,
+    V6,
+}
+fn main() {
+    let four = IpAddrkind::V4;
+    let six = IpAddrkind::V6;
+}
