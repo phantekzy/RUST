56d0a6576fe7945c382fc0bb123893e06440f250
phantekzy <mainilotfi@gmail.com>
2026-09-04 19:40:41 +0100

    I have to review all this again

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ enum IpAddrkind {
     V6,
 }
 fn main() {
-    let four = IpAddrkind::V4;
+    let four = IpAddrkind::V4; // Four and six are of the same type IpAddrkind
     let six = IpAddrkind::V6;
 }
+// We can then , for instance , define a function htat takes any IpAddrkind
+fn route(ip_kind: IpAddrkind) {}
