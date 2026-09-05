154a1727d48c10c320173d038bbf87469813e6fa
phantekzy <mainilotfi@gmail.com>
2026-09-05 10:25:39 +0100

    I have to review all this again

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 // Enum values
 // We can create instances of each of the two variants of IpAddrkind
 enum IpAddrkind {
-    // Custom type
+    // Custom data type
     V4,
     V6,
 }
+
 struct IpAddr {
     // Struct
-    kind: IpAddrkind,
+    kind: IpAddrkind, // Type of IpAddrkind
     address: String,
 }
 fn main() {
@@ -19,5 +20,10 @@ fn main() {
     let _home = IpAddr {
         kind: four,
         address: String::from("127.0.0.1"),
+    };
+    // Loopback
+    let Loopback = IpAddr {
+        kind: six,
+        address: String::from("::1"),
     };
 }
