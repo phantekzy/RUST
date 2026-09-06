3b045a7205019dcc1b224720d6bca6ddc2e23cec
phantekzy <mainilotfi@gmail.com>
2026-09-06 07:49:43 +0100

    Writing it in a concise way

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@
 // if we wanted t o store V4 addresses as four u8 values but still express v6 addresses as a string
 // We wouldn't be able to with a struct
 enum IpAddr {
-    // We attach data to each variant of the enum directly, so there is no need for an extra struct
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from(String::from("::1")));
 }
