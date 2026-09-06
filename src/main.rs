fa11823b358ee7cce74811c28ad211242439dd99
phantekzy <mainilotfi@gmail.com>
2026-09-06 07:25:37 +0100

    Writing it in a concise way

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
 // CUSTOM TYPES
 // Enum values
-// We can create instances of each of the two variants of IpAddrkind
-enum IpAddrkind {
-    // Custom data type
-    V4,
-    V6,
+// We can write it in a better way
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
-struct IpAddr {
-    // Struct
-    kind: IpAddrkind, // Type of IpAddrkind
-    address: String,
-}
-fn main() {
-    // IP Versions
-    let four = IpAddrkind::V4;
-    let six = IpAddrkind::V6;
-    // STORING THE DATA AND IpAddrkind VARIANT OF AN IP ADRESS USING A STRUCT
-    // Home Ip Address v4
-    let _home = IpAddr {
-        kind: four,
-        address: String::from("127.0.0.1"),
-    };
-    // Loopback
-    let Loopback = IpAddr {
-        kind: six,
-        address: String::from("::1"),
-    };
-}
+fn main() {}
