9ceee055c38f05a2af86ad7acac804f4a88a6e7b
phantekzy <mainilotfi@gmail.com>
2026-09-06 07:40:40 +0100

    Writing it in a concise way

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // CUSTOM TYPES
 // Enum values
 // We can write it in a better way
+// if we wanted t o store V4 addresses as four u8 values but still express v6 addresses as a string
+// We wouldn't be able to with a struct
 enum IpAddr {
+    // We attach data to each variant of the enum directly, so there is no need for an extra struct
     V4(String),
     V6(String),
 }
