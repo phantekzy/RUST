7552395f58100c8508348ffcf7772498f7a73ecb
phantekzy <mainilotfi@gmail.com>
2026-09-06 07:59:07 +0100

    same enum but the adress data inside the variants in the form of different structs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 // CUSTOM TYPES
 // Enum values
-// We can write it in a better way
-// if we wanted t o store V4 addresses as four u8 values but still express v6 addresses as a string
-// We wouldn't be able to with a struct
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+
+struct Ipv4Addr {
+    // -- snip
 }
 
-fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from(String::from("::1")));
+struct Ipv6Addr {
+    // -- snip
 }
+
+enum IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+fn main() {}
