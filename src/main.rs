b1b9be343ff44769b3c120d2219c742069f4dd08
phantekzy <mainilotfi@gmail.com>
2026-09-09 21:26:57 +0100

    variety of types embedded

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // CUSTOM TYPES
 // Message enum whose variants each store different amounts and types of values
 enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+    // This Enum has four variants
+    Quit,                       // Quit has no data associated with it at all
+    Move { x: i32, y: i32 },    // Move includes an anonymous struct iniside of it
+    Write(String),              // write includes a single String
+    ChangeColor(i32, i32, i32), // ChangeColor includes three i32 values
 }
+
 fn main() {}
