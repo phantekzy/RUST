107ddfea853c1bc6a33d94e6a5cf8e568483d032
phantekzy <mainilotfi@gmail.com>
2026-09-12 19:56:15 +0100

    What is the option type

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+// OPTION TYPE
 // The option Enum and its Advantages over Null Values
 // In the previous section , we looked at thow the IpAddr enum let us use Rust's type system
 // to encode more information than just the data into our program.
 // This section explores a case study of "Option", which is another enum defined by
 // the Standard Library.
 //
+// The option type is used in many places because it encodes the very common scenario in which value
+// could be something or it could be nothing
 // Main function
 fn main() {}
