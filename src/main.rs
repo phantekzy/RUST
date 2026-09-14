10d4375c5b3df34c4c5e3adb38a96e29b9e47972
phantekzy <mainilotfi@gmail.com>
2026-09-14 07:14:38 +0100

    Option<T> and T are not similars types

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,8 @@
 // OPTION TYPE
-// The option Enum and its Advantages over Null Values
-// In the previous section , we looked at thow the IpAddr enum let us use Rust's type system
-// to encode more information than just the data into our program.
-// This section explores a case study of "Option", which is another enum defined by
-// the Standard Library.
-//
-// The option type is used in many places because it encodes the very common scenario in which value
-// could be something or it could be nothing.
-// Expressing this concept in terms of tye type system means the compiler can check wheter you've
-// handled all the cases you should be handling ; this functionality can prevent bugs that are
-// extremly common in other programming languages.
-//
-// Programming languages design is ofter thought of in terms of which features you include ,
-// but the features you exclude are important too.
-// Rust doesn't have the null feature that many other languages have . Null is a value that means
-// there is no value there
-// In languages with null , variables can always be in one of two states : null or not-null.
-//
-// Rust does not have nulls, but it does have an enum that can encode the concept of a value
-// being present or absent
-// This enum is Option<T> , and it is defined by the standard library as follows :
-// <T> means the Some variant of the Option Enum
-// it can hold one piece of data of any type
-// The Option<T> enum is very usefull that it's even included in the prelude ;
-// We don't need to bring it into scope explicitly.
-//
-// The variants of Option :
-// Some and None
-// We can use Some and None directly without the Option:: prefix .
-// Option enum can hold one piece of data of any type
 // Main function
 fn main() {
-    let some_number = Some(5);
-    let some_string = Some("a String");
-
-    let absent_number: Option<i32> = None;
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // Option<T> and T are different types
+    let sum = x + y;
 }
