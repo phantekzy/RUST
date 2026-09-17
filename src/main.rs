e6d9ef1df5ec35a2f0180f9b1501646237817ac3
phantekzy <mainilotfi@gmail.com>
2026-09-17 21:42:36 +0100

    Option<T> and T are not similars types

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,5 @@
 fn main() {
     let x: i8 = 5;
     let y: Option<i8> = Some(5);
-    // Option<T> and T are different types
-    // Rust doesn't understand how to add an i8 and an Option<T> , because they are different types
     let sum = x + y;
 }
