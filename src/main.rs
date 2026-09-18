39ef04302e866335bbf8b87faa5c38fd27a930a4
phantekzy <mainilotfi@gmail.com>
2026-09-18 21:39:48 +0100

    fixing the unused code

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     // we need to tell Rust what type of Option<T> we have ,
     // because the compiler can't infer the type that
     // the some variant will hold by looking only at None value
-    let absent_number: Option<i32> = None;
+    let _absent_number: Option<i32> = None;
     // Some section
     // In some , we know that a value is present and the
     // value is held within the Some
-    let some_number = Some(5);
-    let some_string = Some("a String");
+    let _some_number = Some(5);
+    let _some_string = Some("a String");
 
     // I have a question
     // When we have a None value , in some sense , it means the
@@ -22,4 +22,6 @@ fn main() {
     // so it means Option<T> it self and The "T" are diff
     // So the compiler won't let us use an Option<T> value
     // as if it were definitely a valid value
+    // Exemple :
+    let 
 }
