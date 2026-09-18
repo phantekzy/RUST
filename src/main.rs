0291dbf9727ac7e5d334ad912158e5d9c097cb54
phantekzy <mainilotfi@gmail.com>
2026-09-18 21:32:23 +0100

    the oposit in Some , we already now about it

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 // Option<T> Enum
 // ain function
 fn main() {
-    // Some section
-    let some_number = Some(5);
-    let some_string = Some("a String");
     // we need to tell Rust what type of Option<T> we have ,
     // because the compiler can't infer the type that
     // the some variant will hold by looking only at None value
     let absent_number: Option<i32> = None;
+    // Some section
+    // In some , we know that a value is present and the
+    // value is held within the Some
+    let some_number = Some(5);
+    let some_string = Some("a String");
 }
