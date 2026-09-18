8d6685e532c9a38ce87b0c2ca10efb68cee8d2b1
phantekzy <mainilotfi@gmail.com>
2026-09-18 21:31:09 +0100

    we need to precise the Option<T> when we use None

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     // Some section
     let some_number = Some(5);
     let some_string = Some("a String");
-    // None section
+    // we need to tell Rust what type of Option<T> we have ,
+    // because the compiler can't infer the type that
+    // the some variant will hold by looking only at None value
     let absent_number: Option<i32> = None;
 }
