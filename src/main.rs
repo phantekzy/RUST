ea626a3b8ae6158f6dcb837d9cf02de9a0472841
phantekzy <mainilotfi@gmail.com>
2026-09-19 11:39:03 +0100

    If we have a value that can be null , we must explicitly opt in by making the type of that value Option<T>

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     let _some_string = Some("a String");
 
     let x: i8 = 5;
-    let y: Option<i8> = Some(5);
+    let y: Option<i8> = Some(5); // In order to have a value that can possibly be null,
+    // we must explicitly opt in by making
+    // the type of that value Option<T>
+
     // We cant add because they are different types
     // We have to convert an Option<T>  to a T before performing T operations with it.
     // This helps catch one of the most common issues with null :
