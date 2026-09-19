ab363153274e58443f14d24188e9abb22a141db5
phantekzy <mainilotfi@gmail.com>
2026-09-19 12:50:37 +0100

    The match Control Flow Operator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // OPTION TYPE
 // Option<T> Enum
 // Match Control Flow Operator
+// Rust has an extremly powerfull control flow operator called match
+// that allows you to compare a value against a series of patterns and then execute code based on
+// which pattern matches.
 // Main function
 fn main() {}
