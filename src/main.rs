05c1987a2510bca693ac59ed6e3757c2fa985052
phantekzy <mainilotfi@gmail.com>
2026-09-19 18:11:48 +0100

    the power of match

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 // OPTION TYPE
 // The match Control Flow Operator
+// Coins Enum
 enum Coin {
     Penny,
     Nickel,
     Dime,
     Quarter,
 }
+// Value in cents function
+fn value_in_cents(coin: Coin) -> u8 {};
+
 // Main function
 fn main() {}
