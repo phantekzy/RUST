818211397221f0a5c730b41d6c8039dbd935168d
phantekzy <mainilotfi@gmail.com>
2026-09-19 18:10:27 +0100

    the power of match

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 // OPTION TYPE
-// Option<T> Enum
-// Match Control Flow Operator
-// Rust has an extremly powerfull control flow operator called match
-// that allows you to compare a value against a series of patterns and then execute code based on
-// which pattern matches.
-// Patterns can be made up of literal values ,
-// variables names , wildcards , and many other things;
-// The power of match comes from the expressiveness of the patterns
-// and the fact that the compiler confirms that all possible cases are handled
+// The match Control Flow Operator
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
 // Main function
 fn main() {}
