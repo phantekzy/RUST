ffa25bd5a1ab355fdc2f8ff8555a04bce1730264
phantekzy <mainilotfi@gmail.com>
2026-09-19 18:14:02 +0100

    match enum exemple

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ enum Coin {
     Quarter,
 }
 // Value in cents function
-fn value_in_cents(coin: Coin) -> u8 {};
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
 
 // Main function
 fn main() {}
