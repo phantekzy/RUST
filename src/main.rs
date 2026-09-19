32feaebc04384c08437bbf1c931834a5aac0a535
phantekzy <mainilotfi@gmail.com>
2026-09-19 18:18:43 +0100

    match expression

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // OPTION TYPE
 // The match Control Flow Operator
 // Coins Enum
+// An enum and a match expression that has
+// the variants of the enum as its patterns
 enum Coin {
     Penny,
     Nickel,
@@ -9,8 +11,9 @@ enum Coin {
 }
 // Value in cents function
 fn value_in_cents(coin: Coin) -> u8 {
-    // Match
+    // Match expression
     match coin {
+        // Patterns
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
