786f4726cd90bfa29576dfddd61e4a24f087c0fe
phantekzy <mainilotfi@gmail.com>
2026-09-19 11:48:47 +0100

    If we have a value that can be null , we must explicitly opt in by making the type of that value Option<T>

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ fn main() {
     let y: Option<i8> = Some(5); // In order to have a value that can possibly be null,
     // we must explicitly opt in by making
     // the type of that value Option<T>
+    // Then, when using that value , we are reauired to explicitly handle the case
+    // when the value is null
+    // Everywhere that a value has a type that isn't an Option<T> , we can safetly assume that the
+    // value isnt null
 
     // We cant add because they are different types
     // We have to convert an Option<T>  to a T before performing T operations with it.
