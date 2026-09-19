f128d490e8fce28766d0265a7a15b3348db5c055
phantekzy <mainilotfi@gmail.com>
2026-09-19 18:06:09 +0100

    im so bored but i have to keep going

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,7 @@
 // Rust has an extremly powerfull control flow operator called match
 // that allows you to compare a value against a series of patterns and then execute code based on
 // which pattern matches.
+// Patterns can be made up of literal values ,
+// variables names , wildcards , and many other things;
 // Main function
 fn main() {}
