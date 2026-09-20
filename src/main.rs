e6d5c7f4d18f3c8093602d686b8d610d81f320c8
phantekzy <mainilotfi@gmail.com>
2026-09-20 21:42:36 +0100

    The two parts of a match arm

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ fn value_in_cents(coin: Coin) -> u8 {
     // Match expression
     match coin {
         // Patterns
-        Coin::Penny => 1,  // Match arms
-        Coin::Nickel => 5, // An arm has two parts : a pattern and some code.
-        Coin::Dime => 10,  // we separate the patern and the code using the "=>" Operator
-        Coin::Quarter => 25,
+        Coin::Penny => 1,    // Match arms
+        Coin::Nickel => 5,   // An arm has two parts : a pattern and some code.
+        Coin::Dime => 10,    // we separate the patern and the code using the "=>" Operator
+        Coin::Quarter => 25, // that separates the pattern and the code to run
     }
     // this seems very similar to an expression used with if ?
     // But there is a big difference :
