fdb0454f328286e4519954c067a1be48078bb63b
phantekzy <mainilotfi@gmail.com>
2026-09-21 22:49:25 +0100

    Patterns That Bind to Values

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 // Patterns That Bind to Values
 // Another useful feature of match arms is that they cn bind to the
 // parts of the values that match the pattern.
+// This is how we can extract values out of enum variants .
+enum UsState {
+    Alabama,
+    Alaska,
+    NewYork,
+    California,
+}
 // Main function
 fn main() {}
