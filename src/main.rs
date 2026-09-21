00478af0ef6a02d69f211ad3805cc26aefbfef8a
phantekzy <mainilotfi@gmail.com>
2026-09-21 22:46:59 +0100

    Patterns That Bind to Values

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // Patterns That Bind to Values
-
+// Another useful feature of match arms is that they cn bind to the
+// parts of the values that match the pattern.
 // Main function
 fn main() {}
