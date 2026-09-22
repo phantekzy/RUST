c7403bf23276451fd859937a697c2e1f13c3a65a
phantekzy <mainilotfi@gmail.com>
2026-09-22 06:34:28 +0100

    Patterns That Bind to Values

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 // Another useful feature of match arms is that they cn bind to the
 // parts of the values that match the pattern.
 // This is how we can extract values out of enum variants .
+//
+// From 1999 through 2008 , the US minted quarters with different designs for each of the 50 states
+// on one side . No other coins got state designs , so only quarters have this extra value .
+// We can add this information to our enum by changing the Quarters variant include a UsState value
+// stored inside of it
+
 #[derive(Debug)] // So we can inspect the state in a minute
 enum UsState {
     Alabama,
