403d3cdecf08af4669064e91cdf1ec84909da2b6
phantekzy <mainilotfi@gmail.com>
2026-09-22 07:39:13 +0100

    Matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,12 @@
 // Let's say we want tot write a function that takes an Option<i32> and , if there's a value inside
 // , adds 1 to that value , If there isn't a value inside , the function should return the None
 // value and not attempt to perform any operations .
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 // Main function
 fn main() {}
