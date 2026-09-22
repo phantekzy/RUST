afc4ce0db046fdfb666147d9f8acbc421cae9dc4
phantekzy <mainilotfi@gmail.com>
2026-09-22 07:31:35 +0100

    Matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 // Matching with Option<T>
+// In the previous section , we wanted to get the inner T value out of t he Some case when using
+// Option<T>; we can also handle Option<T> using match as we did with the Coin enum ! Instead of
+// comparing coins
 // Main function
 fn main() {}
