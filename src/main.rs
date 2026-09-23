fd8be180bf966f495ce769209f232b02020bcac5
phantekzy <mainilotfi@gmail.com>
2026-09-23 20:13:17 +0100

    Matches are exhaustive

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,12 @@
-// Matching with Option<T>
-// In the previous section , we wanted to get the inner T value out of t he Some case when using
-// Option<T>; we can also handle Option<T> using match as we did with the Coin enum ! Instead of
-// comparing coins , we will compare the variants of Option<T> , but the way that the match
-// expression works remains the same .
-//
-// Let's say we want tot write a function that takes an Option<i32> and , if there's a value inside
-// , adds 1 to that value , If there isn't a value inside , the function should return the None
-// value and not attempt to perform any operations .
-
-// A function that uses a match expression on an Option<u32>
+// Matches Are Exhaustive
+// There is another aspect of match , let us consider this version of our plus_one functionthat has
+// a bug and won't compile :
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
-        None => None,
         Some(i) => Some(i + 1),
     }
 }
 
 //
 // Main function
-fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
-// When we call plus_one(five) , the variable x in the body of plus_one will have the value Some(5)
-// We then compare that against each match arm .
-// The Some(5) value doesn't match the pattern None , se we continue to next arm.
-// Does Some(5) match Some(i) ?
-// Why yes it does !
-// We have the same variant . the i binds to the value contained in Some , so i takes the value 5.
-// The code in the match arm is then executed , so we add 1 to the value of i and create a new Some
-// value  with out total 6 inside .
-// Let's consider the second call of plus_one , where x is None .
-// We enter the match and compare to the first arm .
-// IT MATCHES ! there's no value to add to , so the program stops and returns the None value on the
-// right of => . Because the first arm matched , no other arms are compared
-// Combining match and enums is useful in many situations :
-// We will see this pattern a lot in Rust code : match against an enum , bind a variable to the data
-// inside , and then execute code based on it .
+fn main() {}
