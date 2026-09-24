90026c0f21879969010d04e2995b7251652d8d0e
phantekzy <mainilotfi@gmail.com>
2026-09-24 21:38:31 +0100

    How Exhaustiveness works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// Matches Are Exhaustive
 // There is another aspect of match , let us consider this version of our plus_one functionthat has
 // a bug and won't compile :
 fn plus_one(x: Option<i32>) -> Option<i32> {
