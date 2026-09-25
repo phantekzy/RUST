ffee656a236d67762d04cd1671769bd458b747a8
phantekzy <mainilotfi@gmail.com>
2026-09-25 17:51:34 +0100

    If let

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,8 @@ fn main() {
     if let Some(3) = some_u8_value {
         println!("three");
     }
+    // The syntax if let takes a pattern and an expression separated by an equal sign.
+    // It works the same way as a match , where the expression is given to the match and the pattern
+    // is its first arm .
+    // Using if let means less typing , less indentation , and less boilerplate code .
 }
