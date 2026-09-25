cdeab03dc21c2210eded904e56c36b83cffb205d
phantekzy <mainilotfi@gmail.com>
2026-09-25 11:12:07 +0100

    the _ Placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // The _ Placeholder
-//Rust also has a pattern we can use when we don't want to list all possible values .
+// Rust also has a pattern we can use when we don't want to list all possible values .
+// For example , a u8 can have valid values of 0 thriygh 255 . If we only care about the values
+// 1,3,5 and 7 , we dont want tot have to list out 0,2,4,6,8,9 all the way to 255
 // Main function
 fn main() {
     let some_u8_value = Ou8;
