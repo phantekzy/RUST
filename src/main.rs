60f5a86c5a3393086603d46eef169a7d79c3ee0a
phantekzy <mainilotfi@gmail.com>
2026-09-25 16:55:39 +0100

    Concise Control Flow with if let

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@
 //  Main Function
 fn main() {
     let some_u8_value = Some(0u8);
+    // A match that only cares about executing code when the value is Some(3)
     match some_u8_value {
         Some(3) => println!("three"),
         _ => (),
     }
+    // We want t o do sonething with the Some(3) match but do nothing with any other Some<u8> value
+    // or the None value .
+    // To satisfy the match expression , we have to add _ => () after procession just one variant ,
+    // which is a lot of boileerplate code to add .
 }
