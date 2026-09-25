3175ddb26ad737b20584dfe23f59e1efdff13d4b
phantekzy <mainilotfi@gmail.com>
2026-09-25 16:52:03 +0100

    Concise Control Flow with if let

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 // Concise Control Flow with if let
+// The if let syntax lets us combine if and let into a less verbose way to handle values that match
+// one pattern while ignoring the rest .
 
 //  Main Function
-fn main() {}
+fn main() {
+    let some_u8_value = Some(0u8);
+}
